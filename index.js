bc3db47f6c9a7c4c739c14ca5786bf727fe81166
crypticalcoder1 <kirambawycliff@gmail.com>
2024-07-13 12:23:29 +0300

    my file

--- index.js
+++ index.js
@@ -9,15 +9,81 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord = (arr) => {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+        allWagesFor,  // Reference to the function
+    };
+};
+
+const createEmployeeRecords = (data) => {
+    return data.map(createEmployeeRecord);
+};
+
+const createTimeInEvent = function (dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date,
+        hour: parseInt(hour)
+    });
+    return this;
+};
+
+const createTimeOutEvent = function (dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date,
+        hour: parseInt(hour)
+    });
+    return this;
+};
+
+const hoursWorkedOnDate = function (date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date).hour;
+    const timeOut = this.timeOutEvents.find(e => e.date === date).hour;
+    return (timeOut - timeIn) / 100;
+};
+
+const wagesEarnedOnDate = function (date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
+    const eligibleDates = this.timeInEvents.map(e => e.date);
+    
     const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+        return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this), 0); // Binding 'this' for correct context
 
-    return payable
-}
+    return payable;
+};
 
+const findEmployeeByFirstName = (collection, firstName) => {
+    return collection.find(emp => emp.firstName === firstName);
+};
+
+const calculatePayroll = (records) => {
+    return records.reduce((total, record) => {
+        return total + record.allWagesFor();
+    }, 0);
+};
+
+// Exporting all functions for testing
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll,
+};
